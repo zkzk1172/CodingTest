f493add3a078671215383a8ff99313c31aed3b4f
zkzk1172 <80510956+zkzk1172@users.noreply.github.com>
2025-04-12 13:17:23 +0900

    Create 문자열.py

---
+++ 문자열.py
@@ -0,0 +1,21 @@
+#문자열 작은 따옴표에도 담을수있음
+a='hello'
+print(a)
+
+#큰따옴표로 문자를 담았다면 내부적으로 작은따옴표 사용가능
+a="I'm jy"
+print(a)
+
+#마찬가지로 작은 따옴표로 문자를 담았다면 큰까옴표 사용가능
+a='"welcome"'
+print(a)
+
+#문자열 더하기
+b='"the world"'
+print(a+" "+b)
+
+#문자열 여러번 출력하기
+print(b*3)
+
+a="abcdefg"
+print(a[1:6])
